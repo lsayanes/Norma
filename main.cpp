d16f28391fb73bac87a7dd88bf9ada0bb8540e8d
lsayanes <leandro.sayanes@gmail.com>
2026-04-05 14:17:30 -0300

    icon

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <QApplication>
 #include <QStyleFactory>
+#include <QIcon>
+#include <QFileInfo>
 #include <string>
 #include <iostream>
 
@@ -10,12 +12,17 @@ int main(int argc, char *argv[])
     QApplication app(argc, argv);
 
     const char *appName =  "Norma";
-    
+
     app.setApplicationName(appName);
     app.setApplicationVersion("1.1");
     app.setOrganizationName("iDev - JalaGamaes");
 
     app.setStyle(QStyleFactory::create("Fusion"));
+
+    // Icono de aplicacion y dock (debe setearse en QApplication, no en la ventana)
+    const QString iconPath = QApplication::applicationDirPath() + "/resources/Norma.png";
+    if (QFileInfo::exists(iconPath))
+        app.setWindowIcon(QIcon(iconPath));
 
     Norma NormaApp;
     bool bCreated = NormaApp.create(appName);
